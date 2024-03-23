aa00538ae79af9db0a1b94444798bcac48dda662
piratewiz <mxkst.99@gmail.com>
2024-03-23 21:56:17 +0100

    correciones finales proyecto

---
+++ products.js
@@ -0,0 +1,134 @@
+export const products = [
+  {
+    name: 'White Basic Shirt',
+    price: 29.95,
+    nature: {
+      color: ['White']
+    },
+    gender: 'Male',
+    seller: 'Zara',
+    image:
+      'https://static.zara.net/assets/public/6a6a/23c5/4ffd46768d15/a2b272e97155/00962458251-a1/00962458251-a1.jpg?ts=1707477943829&w=824'
+  },
+  {
+    name: 'Polo Marine Style',
+    price: 29.95,
+    nature: {
+      color: ['White', 'Black']
+    },
+    gender: 'Male',
+    seller: 'Zara',
+    image:
+      'https://static.zara.net/assets/public/3488/b1fa/d0e942a4b35e/5a65b31d66c9/1684935592365/1684935592365.jpg?ts=1704328686958&w=364'
+  },
+  {
+    name: 'Nappa Brown Jacket',
+    price: 99.95,
+    nature: {
+      color: ['Brown']
+    },
+    gender: 'Male',
+    seller: 'Massimo Dutti',
+    image:
+      'https://i.pinimg.com/564x/b2/44/57/b244578290908b744b0253e9c76027f4.jpg'
+  },
+  {
+    name: 'Syntetic Black Jacket',
+    price: 45.95,
+    nature: {
+      color: ['Black']
+    },
+    gender: 'Female',
+    seller: 'Massimo Dutti',
+    image:
+      'https://i.pinimg.com/564x/d4/0c/cd/d40ccda1d767dc96a5b2b597b201bf74.jpg'
+  },
+  {
+    name: 'White Cotton Shirt',
+    price: 45.95,
+    nature: {
+      color: ['White']
+    },
+    gender: 'Female',
+    seller: 'Massimo Dutti',
+    image:
+      'https://i.pinimg.com/564x/ca/33/33/ca33333ac77289a7a90a9e643577a21d.jpg'
+  },
+  {
+    name: 'Reversible Bomber Jacket',
+    price: 60.95,
+    nature: {
+      color: ['Brown', 'Black']
+    },
+    gender: 'Male',
+    seller: 'Levis',
+    image:
+      'https://i.pinimg.com/564x/1a/ed/af/1aedaf327c569cb2a6dc6f72faf9827f.jpg'
+  },
+  {
+    name: 'Battery Crewneck Sweater',
+    price: 45.95,
+    nature: {
+      color: ['Blue', 'Black', 'White']
+    },
+    gender: 'Male',
+    seller: 'Levis',
+    image:
+      'https://i.pinimg.com/564x/27/8d/02/278d02b5ce2561e9de9d1a548cd1945a.jpg'
+  },
+  {
+    name: 'Wilder Flower Trucker Jacket',
+    price: 60.95,
+    nature: {
+      color: 'Blue'
+    },
+    gender: 'Female',
+    seller: 'Levis',
+    image:
+      'https://i.pinimg.com/564x/12/94/b7/1294b72b2c661f8e73ca596f93626c54.jpg'
+  },
+  {
+    name: 'Womans ColdGear Shirt',
+    price: 30.95,
+    nature: {
+      color: 'Black'
+    },
+    gender: 'Female',
+    seller: 'Under Armour',
+    image:
+      'https://i.pinimg.com/564x/d8/49/b0/d849b0401e83c008d60cc269c13183ad.jpg'
+  },
+  {
+    name: 'Trek Polar Fleece Full Zip',
+    price: 120.95,
+    nature: {
+      color: ['Black', 'Blue', 'White']
+    },
+    gender: 'Female',
+    seller: 'Under Armour',
+    image:
+      'https://i.pinimg.com/564x/42/8f/97/428f97422e10bb5afc0f7623c3c5db88.jpg'
+  },
+  {
+    name: 'Autumn Beis Coat',
+    price: 199.95,
+    nature: {
+      color: 'Beis'
+    },
+    gender: 'Female',
+    seller: 'Massimo Dutti',
+    image:
+      'https://i.pinimg.com/564x/a3/c3/2a/a3c32af68598a512f886a852946962c9.jpg'
+  },
+  {
+    name: 'Autumn Black Coat',
+    price: 199.95,
+    nature: {
+      color: ['Blue', 'Black']
+    },
+    gender: 'Male',
+    seller: 'Massimo Dutti',
+    image:
+      'https://i.pinimg.com/564x/4e/53/cd/4e53cdae5f5c198afcf1115f1dbd8bd8.jpg'
+  }
+]
